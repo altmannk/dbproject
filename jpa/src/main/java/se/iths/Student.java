ec1101d50a83d4056d8deb5f1c5f2cb462da17b1
Linus Andreasson <lmmandreasson@gmail.com>
2023-12-18 12:34:39 +0100

    Student added

---
+++ jpa/src/main/java/se/iths/Student.java
@@ -0,0 +1,44 @@
+package se.iths;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "studentID", nullable = false)
+    private Integer id;
+
+    @Column(name = "studentName", length = 50, unique = true)
+    private String studentName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studentCourseID")
+    private Course studentCourseID;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Course getStudentCourseID() {
+        return studentCourseID;
+    }
+
+    public void setStudentCourseID(Course studentCourseID) {
+        this.studentCourseID = studentCourseID;
+    }
+
+}
