538e628aa71b672b6583ba6efc8faed397ef0df6
cybertobbe <143131053+cybertobbe@users.noreply.github.com>
2023-12-18 12:28:05 +0100

    Add Teacher entity

---
+++ jpa/src/main/java/se/iths/Teacher.java
@@ -0,0 +1,32 @@
+package se.iths;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teacher")
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "teacherID", nullable = false)
+    private Integer id;
+
+    @Column(name = "teacherName", length = 50)
+    private String teacherName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+}
