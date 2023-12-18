3f1cadd09dc2b5ff7453ba094807adfdcfa0d45f
August <august.rydnell@gmail.com>
2023-12-18 11:10:37 +0100

    august måndag 12-18

---
+++ jpa/src/main/java/se/iths/Program.java
@@ -0,0 +1,43 @@
+package se.iths;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "program")
+public class Program {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "programID", nullable = false)
+    private Integer id;
+
+    @Column(name = "programName", length = 50, unique = true)
+    private String programName;
+
+    @Column(name = "programYear")
+    private Integer programYear;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProgramName() {
+        return programName;
+    }
+
+    public void setProgramName(String programName) {
+        this.programName = programName;
+    }
+
+    public Integer getProgramYear() {
+        return programYear;
+    }
+
+    public void setProgramYear(Integer programYear) {
+        this.programYear = programYear;
+    }
+
+}
